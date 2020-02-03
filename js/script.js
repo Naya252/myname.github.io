393ed7a34b6ac8c1076c6866ac31d6e83e8360b2
Naya252 <52450582+Naya252@users.noreply.github.com>
2020-02-03 06:13:08 +0300

    modal 50/50
    modal 50/50

--- js/script.js
+++ js/script.js
@@ -1,23 +1,94 @@
-// // модальное окно
-let btn = document.getElementById('btn');
-let btn2 = document.getElementById('btn2');
-let formNav = document.getElementById('form-nav');
+// пример карточки товара, при наведении на кнопку
+let catalogBtn = document.getElementById('catalogBtn');
+let catalogClass = document.getElementById('catalogClass');
+let catalogCard = document.getElementById('catalog__card');
+
+catalogCard.onmouseover = function () {   
+    catalogClass.style.borderWidth = "1px";
+    catalogClass.style.borderStyle = "solid";
+    catalogClass.style.borderColor = "#CBA696";
+};
+
+catalogCard.onmouseout = function () {
+    catalogClass.style.borderWidth = "1px";
+    catalogClass.style.borderStyle = "solid";
+    catalogClass.style.borderColor = "transparent";
+};
+
+//пример модального окна
+let catalogMod = document.getElementById('catalogMod');
 let close = document.getElementById('close');
 
-/* Открываем модальное окно */
-btn.onclick = function () {
-    btn.setAttribute("href", "#form-nav"); 
+// Открываем модальное окно 
+catalogBtn.onclick = function () {
+    catalogBtn.setAttribute("href", "#catalogMod"); 
     document.body.style.overflow = "hidden";
+    // document.body.classList.add("blur");
 };
-btn2.onclick = function () {
-    btn2.setAttribute("href", "#form-nav"); 
-    document.body.style.overflow = "hidden";
-};
-/* Закрываем модальное окно */
+// Закрываем модальное окно 
 close.onclick = function () {
-    btn.removeAttribute("href"); 
+    catalogBtn.removeAttribute("href"); 
     document.body.style.overflow = null;
+    // document.body.classList.remove("blur");
 };
+
+
+//tooltip info
+(function() {
+    // Get all the toggletip buttons
+    var toggletips = document.querySelectorAll('[data-toggletip-content]');
+  
+    // Iterate over them
+    Array.prototype.forEach.call(toggletips, function (toggletip) {
+      // Get the message from the data-content element
+      var message = toggletip.getAttribute('data-toggletip-content');
+  
+      // Get the live region element
+      var liveRegion = toggletip.nextElementSibling;
+  
+      // Toggle the message
+      toggletip.addEventListener('click', function () {
+          liveRegion.innerHTML = '';
+          window.setTimeout(function() {
+            liveRegion.innerHTML = '<span class="toggletip-bubble">'+ message +'</span>';
+          }, 100);
+      });
+  
+      // Close on outside click
+      document.addEventListener('click', function (e) {
+        if (toggletip !== e.target) {
+          liveRegion.innerHTML = '';
+        }                        
+      });
+  
+      // Remove toggletip on ESC
+      toggletip.addEventListener('keydown', function (e) {
+        if ((e.keyCode || e.which) === 27)
+        liveRegion.innerHTML = '';
+      });
+      
+      // Remove on blur
+      toggletip.addEventListener('blur', function (e) {
+        liveRegion.innerHTML = '';
+      });
+    });
+  }());
+
+//map
+var mymap = L.map('mapid').setView([57.757401, 40.986840], 15.5);
+var myMarker = L.icon({
+    iconUrl: 'img/marker3.png',
+    iconSize:     [32, 43], // size of the icon
+    iconAnchor:   [12, 55], // point of the icon which will correspond to marker's location
+});
+var marker = L.marker([57.757401, 40.986840], {icon: myMarker}).addTo(mymap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibmF5YTI1MiIsImEiOiJjazY1MWI5ODQwa25jM21vN3dvMnJvbDB4In0.ex-oBkf9ssYk4h-xuua1gg'
+}).addTo(mymap);
 
 
 
